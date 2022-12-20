f9206bb3bc8b632e55abe4fac2dd459c0dd363fa
clon54programer <112908720+clon54programer@users.noreply.github.com>
2022-12-20 18:33:19 -0500

    coloque una variable cliente y le colorque valor a name

--- java/practica.java
+++ java/practica.java
@@ -2,7 +2,10 @@ public class practica{
     //la public class deber tener el mismo nombre que el archivo
     public static void main(String[] args){
       double name;
-      System.out.println("hello world");
+      String Cliente = "juna danilo";//los datos tipos string debe ir en mayusculoas 
+      System.out.println("hola señor"+Cliente+" ¿puede decirme su edad? por favor");
+
+    name = 1.75D;
      
     }
 }
