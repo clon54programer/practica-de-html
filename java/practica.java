3d3004fac37f3984f5455d48f7ee07a4c67d0b7a
clon54programer <112908720+clon54programer@users.noreply.github.com>
2022-12-20 18:28:50 -0500

    cree un archivo java para practicar el lenguaje y probar git

---
+++ java/practica.java
@@ -0,0 +1,8 @@
+public class practica{
+    //la public class deber tener el mismo nombre que el archivo
+    public static void main(String[] args){
+      double name;
+      System.out.println("hello world");
+     
+    }
+}
